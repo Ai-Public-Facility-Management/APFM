16fb246c8db8d73817ae785c0397cf4475ca276c
LChaeHyun <94784148+LChaeHyun@users.noreply.github.com>
2025-07-30 02:38:49 +0000

    패키지 구조 수정

--- backend/src/main/java/server/controller/AdminController.java
+++ backend/src/main/java/server/controller/AdminController.java
@@ -2,7 +2,7 @@ package server.controller;
 
 import server.domain.ApprovalStatus;
 import server.domain.Users;
-import server.domain.UserRepository;
+import server.domain.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
