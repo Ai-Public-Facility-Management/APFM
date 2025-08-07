a3a88fca0bf4112ff57cd544844ee7200f2be15a
LeeChaeHyun <good4170@gmail.com>
2025-08-07 13:43:18 +0900

    EnableScheduling 추가

--- backend/src/main/java/server/BackendApplication.java
+++ backend/src/main/java/server/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import server.config.kafka.KafkaProcessor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 @EntityScan(basePackages = "server.domain") 
 @EnableJpaRepositories("server.repository")
+@EnableScheduling
 public class BackendApplication {
 
     public static ApplicationContext applicationContext;
