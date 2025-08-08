095755a918ca757f157a52a564efb1749a36920b
dnjsdn752 <dnjsdn752@naver.com>
2025-08-08 11:29:06 +0900

    ai제안서작성

--- AI/run.py
+++ AI/run.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
 from ultralytics import YOLO
 from PIL import Image
 import io
