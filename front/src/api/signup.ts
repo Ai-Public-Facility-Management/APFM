0cc00133a3b960958e3b23d7e99a521cf03f1aab
minsu-jeong123 <81951299+minsu-jeong123@users.noreply.github.com>
2025-08-08 00:37:41 +0000

    signup 수정

--- front/src/api/signup.ts
+++ front/src/api/signup.ts
@@ -9,13 +9,13 @@ export interface SignUpRequest {
 }
 
 export const sendVerificationCode = async (email: string) => {
-  return await axios.post("http://localhost:8082/api/auth/send-code", { email });
+  return await axios.post(`/api/auth/send-code?email=${email}`);
 };
 
 export const verifyCode = async (email: string, code: string) => {
-  return await axios.post(`http://localhost:8082/api/auth/verify-code?email=${email}&code=${code}`);
+  return await axios.post(`/api/auth/verify-code?email=${email}&code=${code}`);
 };
 
 export const submitSignUp = async (data: SignUpRequest) => {
-  return await axios.post("http://localhost:8082/api/auth/signup", data);
+  return await axios.post(`/api/auth/signup`, data);
 };
