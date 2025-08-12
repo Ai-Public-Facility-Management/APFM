6edcff6b744b1f5fc032af2001159079b2d4dc8d
LeeChaeHyun <good4170@gmail.com>
2025-08-12 14:47:18 +0900

    카메라 이름도 추가

--- backend/src/main/java/server/service/CameraService.java
+++ backend/src/main/java/server/service/CameraService.java
@@ -7,9 +7,8 @@ import server.dto.CameraResponse;
 import server.repository.CameraRepository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
