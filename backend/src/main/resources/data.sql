93eb603c225ec1c93d6ff69b945e9ce7f937f3a6
LeeChaeHyun <good4170@gmail.com>
2025-08-13 13:59:07 +0900

    data.sql 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -18,19 +18,40 @@ INSERT INTO Inspection_Setting (
     7
 );
 
--- insert into INSPECTION(id,CREATE_DATE ) values (1,'2025-08-01');
---
--- insert into CAMERA(id,ip,location,latitude,longitude) values (1,'testIP_1','부산역 5번출구',35.115012160083616,129.03920684658144);
---
---
--- INSERT INTO PUBLIC_FA(ID,INSTALL_DATE,LAST_REPAIR,OBSTRUCTION,SECTION_HEIGHT,  SECTION_WIDTH ,SECTION_X_CENTER ,SECTION_Y_CENTER, STATUS ,TYPE , CAMERA_ID ) VALUES (3,'2025-08-01','2025-08-01',100,1,1,1,1,'ABNORMAL','BENCH',1);
+insert into INSPECTION(id,CREATE_DATE ) values (1,'2025-08-01');
 
--- INSERT INTO issue (
---     id, content, creation_date, description, estimate, estimate_basis,
---     image_description, image_url, location, obstruction_level, result_id,
---     status, type, inspection_id, public_fa_id
--- ) VALUES
--- (2, 'Content 2', '2025-08-01', 'Description for TYPE_B', 20000, 'Basis B', 'Image desc B', 'http://example.com/image2.jpg', 'Busan', 3, 102, 'REPAIR', 'DAMAGE', 1, 3);
+insert into CAMERA(id,ip,location,latitude,longitude,IMAGE_DESCRIPTION,image_url) values (1,'testIP_1','부산역 5번출구',35.115012160083616,129.03920684658144,'image','test_img_url');
 
+INSERT INTO PUBLIC_FA(ID,INSTALL_DATE,LAST_REPAIR,OBSTRUCTION,SECTION_HEIGHT,SECTION_WIDTH ,SECTION_X_CENTER ,SECTION_Y_CENTER, STATUS ,TYPE ,CAMERA_ID, IMAGE_DESCRIPTION,image_url) VALUES (1,'2025-08-01','2025-08-01',100,1,1,1,1,'ABNORMAL','BENCH',1,'image','test_img_url');
+
+INSERT INTO issue (
+    id,
+    creation_date,
+    estimate,
+    estimate_basis,
+    estimate_references,
+    obstruction,
+    obstruction_basis,
+    result_id,
+    status,
+    type,
+    vision_analysis,
+    inspection_id,
+    public_fa_id
+) VALUES (
+             1,
+             '2025-08-13 14:30:00.000000',
+             120000,
+             '현장 조사 결과 기반',
+             '설계 문서 #23',
+             3,
+             '도로 폐쇄 수준',
+             101,
+             'REPAIR',
+             'DAMAGE',
+             'AI 분석 결과 이상 없음',
+             1,
+             1
+         );
 
 
